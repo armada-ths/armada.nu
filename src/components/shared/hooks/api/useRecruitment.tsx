7ea6924a428665fc7fcd407fdb4713b5f8ec94f1
neonguyen-dev <neo20030125@gmail.com>
2025-03-30 11:13:36 +0200

    fix: fl ot recruitment

--- src/components/shared/hooks/api/useRecruitment.tsx
+++ src/components/shared/hooks/api/useRecruitment.tsx
@@ -28,68 +28,61 @@ export async function fetchRecruitment(options?: RequestInit) {
   return result[0] as Recruitment*/
 
   //returning null when recruitment is not open
-  return null
 
   const staticRecruitment: Recruitment = {
-    name: "Marketing & Communications - Operation Team Recruitment",
-    link: "https://app.eventro.se/recruitments/b55c85c1-347b-4f54-bdb8-c4deb4b24806",
-    start_date: "2025-02-28",
-    end_date: "2025-03-24",
+    name: "Fair and Logistics - Operation Team Recruitment",
+    link: "https://app.eventro.se/recruitments/edbf34d6-9bdc-4212-9a7e-b5e73e6eb009",
+    start_date: "2025-03-31",
+    end_date: "2025-04-28",
     groups: {
-      "Marketing & Communications Operating Team": [
+      "Fair & Logistics Operating Team": [
         {
-          name: "Social Media Manager",
+          name: "Talent Coordinator",
           parent: null,
           description:
-            "As Social Media Manager, you will be responsible for bringing Armada to life online through Instagram, Facebook, and TikTok. Your main tasks include planning and executing marketing campaigns, engaging with followers, and creating short-form content that offers insight into the Armada organization. You will work closely with the Head of Media & Marketing and the graphic design team to maintain a strong and creative online presence. This role is ideal for someone who is proactive, outgoing, innovative and passionate about digital communication."
+            "As a Talent Coordinator, you will lead the host recruitment drive in the fall, working with the Head of  Human Resources & Diversity and collaborating with the Events and Marketing teams to find and match candidates to suitable roles. You will also organize the Armada Party, the grand celebration at the end of the fair. Strong communication and interpersonal skills are essential, as this role involves coordinating across teams to ensure a smooth recruitment process and a successful event."
         },
         {
-          name: "Team Leader: Photo ",
+          name: "Diversity Coordinator",
           parent: null,
           description:
-            "As Photo Team Coordinator, you will take the lead in capturing and curating Armada’s visual identity. In the spring, you will serve as Armada’s primary photographer, and in the autumn, you will lead a team to document all major events, including the fair, panels, and banquet. Your work will contribute to the website, social media, and various campaigns. You will collaborate closely with the film team to ensure cohesive visual storytelling. This role is ideal for someone who is detail oriented and a good planner. An interest in photography, photo editing, and creative direction is essential for this role."
+            "As the Diversity Coordinator, you will drive Armada’s commitment to inclusivity, equality, and diversity in collaboration with the Head of Sustainability and  Head of  Human Resources & Diversity. Your role will span recruitment, team-building, media engagements, and more, ensuring diversity is embedded across all initiatives. Additionally, you will lead the organization of Diversity Day & Room, Armada’s key events. An open-minded approach, willingness to challenge norms, and a strong passion for diversity are essential to making a meaningful impact in this role."
         },
         {
-          name: "Team Leader: Film Crew Coordinator",
+          name: "Team Leader: University Relations",
           parent: null,
           description:
-            "As Film Crew Coordinator, you will lead Armada’s film team, overseeing the creation of video content that captures the spirit of the event. Your responsibilities include planning and organizing projects, coordinating the team, and ensuring high-quality production. You will work closely with a dynamic group to produce everything from short-form videos for social media to larger branding films. A strong interest in video production, teamwork, and storytelling is key. Previous experience in filming and editing is meritorious."
+            "This role requires you to work closely with the Talent coordinators to support the host recruitment drive during fall for Armada. Therefore, you will also co-manage the coffee campaigns and assist in team-building activities during the recruitment drive. During the fair, you will be responsible for hosting the other universities taking part in the fair and organizing the alternative Banquet with the help of your hosts. Creativity is crucial and you need to be good at organizing and delegating tasks for this role."
         },
         {
-          name: "Team Leader: Film Crew ",
+          name: "Team Leader: Host Coordinator",
           parent: null,
           description:
-            "As part of Armada’s Film Crew, you will play a key role in creating video content for the organization. The team will work on a variety of projects, ranging from short promotional videos for social media to larger branding films, including the highly anticipated banquet video. You will collaborate closely with a small team to bring Armada’s vision to life through engaging visual storytelling. A passion for filmmaking, creativity, and teamwork is essential. Experience in video recording and editing is meritorious."
+            "As a Host Coordinator, you will be responsible for the recruitment and management of the host team. You will work closely with the Talent Coordinators to ensure a smooth recruitment process and a successful event. Strong communication and interpersonal skills are essential, as this role involves coordinating across teams to ensure a smooth recruitment process and a successful event."
         },
         {
-          name: "Graphic Designer",
+          name: "Team Leader: Operations",
           parent: null,
           description:
-            "As a Graphic Designer of Armada 2025 you will be part of a dynamic and creative team, collaborating with several stakeholders. Expanding upon the established visual identity for this year’s edition, you'll have the autonomy to infuse your creativity into each design, encouraging you to explore and acquire new skills to enhance your qualities. In the team you will get to come up with innovative ideas, create iterations and refine concepts to deliver outstanding visual solutions. It’s your opportunity to make your imaginations come to life in ways that inspire and engage to not only meet but exceed the expectations of students and companies alike, reflecting the vision of our team and overarching goal to make this the most visually striking edition of Armada to date."
+            "As the Team Leader of Logistics  within the Banquet team, your main responsibilities are to ensure that the event, the  preparations and the post-event breakdown of the banquet will be as smooth as possible. This includes transportation of goods, decorations and personnel to and from the venue. You’ll be responsible for a small team of banquet hosts during the event. Structure, independence, and communication is key for this role, as well as being able to adapt to unexpected situations."
         },
         {
-          name: "Frontend Developer",
+          name: "Team Leader: Creative",
           parent: null,
           description:
-            "As a web developer for Armada, you will work with front-end focused web development and be in a team with three other web developers, one UX/UI designer, and one Head of Web Development. In addition to this, you will be part of the larger team. As a front-end developer, you will work very closely with the back-end team as well. \n\n You are creative, have a passion for building things with the user in mind, and most importantly have a strong interest in learning more about front-end web development. Your passion and engagement are highly valued in combination with good collaboration skills. You will be working with HTML5, CSS, Javascript, React, and data fetching from API:s and Github as a version control tool. We will also work extensively with documentation using something similar to Storybook for our components. Having these skills is meritorious, but not required to have. Our main focus is that you should have fun while developing some awesome stuff and use this as an opportunity to learn more and gain valuable developer experience!"
+            "As the Team Leader of Creative within the Banquet team, your main responsibility will be to shape the banquet to fit the chosen theme. You will be expected to stay within a given budget. During the day of the event, you will be responsible over a small team of banquet hosts and will be responsible for the set up and breakdown of the venue, aided by the banquet operations team. Structure, independence, and communication is key for this role, but creativity will also be merited."
         },
         {
-          name: "UX/UI Designer",
+          name: "Team Leader: Entertainment",
           parent: null,
           description:
-            "As a UX/UI designer at Armada, you will be designing and enabling features to improve the user experience both on Armada's website as well as in our internal systems. Your designs will be the backbone to updating and creating new content for Armada’s experiences. You will be a part of the web team responsible for the website. \n\nYou are creative, have a passion for designing with the user in mind and most importantly have a strong interest in user experience design. Your passion and engagement are highly valued in combination with good communication skills. Prior familiarity with using digital design tools such as Figma is valued and knowledge about atomic design methodology, but the most important thing is that you are committed and eager to learn."
+            "As the Team Leader of Entertainment within the Banquet team, your main responsibility will be to find and book a master of ceremonies, entertainment and performers for the event. You will also be expected to plan and structure the banquet schedule in detail while staying within budget. Structure, independence, communication and drive is crucial for success in this role."
         },
         {
-          name: "Backend Developer",
+          name: "Coordinator",
           parent: null,
           description:
-            "As a backend developer, your primary responsibility will be to maintain and improve THS Armada’s internal IT systems. The systems support the recruitment process, tickets and CRM to mention some areas. You will be a part of the web team responsible for the website. \n\nThe backend developers will take part in the creation of new functionality with the ambition to provide the best digital experience possible for the end users. It will be possible to do some front-end work as well. As a backend developer. You will be working with Python, Django, HTML/CSS, Github, and PostgreSQL databases. Prior knowledge is valued, but the most important thing is that you are committed and eager to learn."
-        },
-        {
-          name: "Fullstack Developer",
-          parent: null,
-          description:
-            "As a fullstack developer, you are a jack of all trades when it comes to the development of an application but not a master.  You will serve both the backend and frontend when needed but work mostly with integration to the user end. You will be part of the web team responsible for the website. \n\n The fullstack developers will take part in the creation of new functionality with the ambition to provide the best digital experience possible for the end users. It will be possible to do some front-end work as well. As a fullstack developer. You will be working with Python, Django, HTML/CSS, Github, PostgreSQL, HTML5, CSS, Javascript, React, and data fetching from API:s. Prior knowledge is valued, but the most important thing is that you are committed and eager to learn."
+            "As the Coordinator in the Banquet team, your main responsibility will be managing everything related to the guests. With support from the rest of the team you will be in charge of planning the seating arrangement, putting together a guest list for both the banquet and the after party, and seeing to the guest's dietary needs. Structure, independence and communication is key for this role."
         }
       ]
     }
