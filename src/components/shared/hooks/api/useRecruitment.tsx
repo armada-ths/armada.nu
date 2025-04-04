35d2798d378ada507bebfd087998a34a95b51a73
Durelius <wilhelm@durelius.se>
2025-04-04 16:24:02 +0200

    Added banquet prefix to banquet roles

--- src/components/shared/hooks/api/useRecruitment.tsx
+++ src/components/shared/hooks/api/useRecruitment.tsx
@@ -97,25 +97,25 @@ export async function fetchRecruitment(options?: RequestInit) {
             "This role requires you to work closely with the Talent coordinators to support the host recruitment drive during fall for Armada. Therefore, you will also co-manage the coffee campaigns and assist in team-building activities during the recruitment drive. During the fair, you will be responsible for hosting the other universities taking part in the fair and organizing the alternative Banquet with the help of your hosts. Creativity is crucial and you need to be good at organizing and delegating tasks for this role."
         },
         {
-          name: "Team Leader: Operations",
+          name: "Banquet Team Leader: Operations",
           parent: null,
           description:
             "As the Team Leader of Logistics  within the Banquet team, your main responsibilities are to ensure that the event, the  preparations and the post-event breakdown of the banquet will be as smooth as possible. This includes transportation of goods, decorations and personnel to and from the venue. You’ll be responsible for a small team of banquet hosts during the event. Structure, independence, and communication is key for this role, as well as being able to adapt to unexpected situations."
         },
         {
-          name: "Team Leader: Creative",
+          name: "Banquet Team Leader: Creative",
           parent: null,
           description:
             "As the Team Leader of Creative within the Banquet team, your main responsibility will be to shape the banquet to fit the chosen theme. You will be expected to stay within a given budget. During the day of the event, you will be responsible over a small team of banquet hosts and will be responsible for the set up and breakdown of the venue, aided by the banquet operations team. Structure, independence, and communication is key for this role, but creativity will also be merited."
         },
         {
-          name: "Team Leader: Entertainment",
+          name: "Banquet Team Leader: Entertainment",
           parent: null,
           description:
             "As the Team Leader of Entertainment within the Banquet team, your main responsibility will be to find and book a master of ceremonies, entertainment and performers for the event. You will also be expected to plan and structure the banquet schedule in detail while staying within budget. Structure, independence, communication and drive is crucial for success in this role."
         },
         {
-          name: "Coordinator",
+          name: "Banquet Coordinator",
           parent: null,
           description:
             "As the Coordinator in the Banquet team, your main responsibility will be managing everything related to the guests. With support from the rest of the team you will be in charge of planning the seating arrangement, putting together a guest list for both the banquet and the after party, and seeing to the guest's dietary needs. Structure, independence and communication is key for this role."
