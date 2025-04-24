e13c2b03159b237040a0df71ce72525037e2ea18
neonguyen-dev <neo20030125@gmail.com>
2025-04-24 21:11:14 +0200

    test with experimental runtime

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 
 export const env = createEnv({
-  runtimeEnv: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  experimental__runtimeEnv: {
     SLACK_SALES_HOOK_URL: process.env.SLACK_SALES_HOOK_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY
   }
   // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
