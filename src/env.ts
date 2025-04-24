f5aae1c09bfe2d15d65bef5c332f216d4bea29ac
neonguyen-dev <neo20030125@gmail.com>
2025-04-24 21:31:06 +0200

    ref: changed from t3-oss envs for type errors

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,14 @@
-import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
-export const env = createEnv({
-  server: {
-    SLACK_SALES_HOOK_URL: z.string().min(1)
-  },
-  client: {
-    NEXT_PUBLIC_API_URL: z.string().min(1),
-    NEXT_PUBLIC_RECAPTCHA_KEY: z.string().min(1)
-  },
-  runtimeEnvStrict: {
-    SLACK_SALES_HOOK_URL: process.env.SLACK_SALES_HOOK_URL,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY
-  }
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-  // For Next.js >= 13.4.4, you only need to destructure client variables:
-  /* 	experimental__runtimeEnv: {
-		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_PUBLIC_API_URL
-	} */
+const envSchema = z.object({
+  SLACK_SALES_HOOK_URL: z.string().min(1),
+
+  NEXT_PUBLIC_API_URL: z.string().min(1),
+  NEXT_PUBLIC_RECAPTCHA_KEY: z.string().min(1)
+})
+
+export const env = envSchema.parse({
+  SLACK_SALES_HOOK_URL: process.env.SLACK_SALES_HOOK_URL,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY
 })
