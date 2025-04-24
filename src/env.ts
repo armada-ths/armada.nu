7f00d6592ad2d898e305c6c629612cf1d93dc68d
neonguyen-dev <neo20030125@gmail.com>
2025-04-24 21:41:56 +0200

    ref: easier validation

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,9 @@
-import { z } from "zod"
+const getEnvVar = (key: string, defaultValue = "") => {
+  return process.env[key] || defaultValue
+}
 
-const envSchema = z.object({
-  SLACK_SALES_HOOK_URL: z.string().min(1),
-
-  NEXT_PUBLIC_API_URL: z.string().min(1),
-  NEXT_PUBLIC_RECAPTCHA_KEY: z.string().min(1)
-})
-
-export const env = envSchema.parse({
-  SLACK_SALES_HOOK_URL: process.env.SLACK_SALES_HOOK_URL,
-  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  NEXT_PUBLIC_RECAPTCHA_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_KEY
-})
+export const env = {
+  SLACK_SALES_HOOK_URL: getEnvVar("SLACK_SALES_HOOK_URL"),
+  NEXT_PUBLIC_API_URL: getEnvVar("NEXT_PUBLIC_API_URL"),
+  NEXT_PUBLIC_RECAPTCHA_KEY: getEnvVar("NEXT_PUBLIC_RECAPTCHA_KEY")
+}
