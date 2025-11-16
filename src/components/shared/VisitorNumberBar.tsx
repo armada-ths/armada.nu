5f203cbe412a1394b451691b440b6f493790c262
Einar Harri <eharri@kth.se>
2025-11-16 14:43:35 +0100

    feat: visual effect on countdown end

--- src/components/shared/VisitorNumberBar.tsx
+++ src/components/shared/VisitorNumberBar.tsx
@@ -1,18 +1,18 @@
 import { NumberCountUp } from "@/app/exhibitor/_components/NumberCountUp"
 
-export function VisitorNumberBar() {
+export function VisitorNumberBar({ disableAnimation = false }: { disableAnimation?: boolean }) {
   return (
     <div className="flex w-full flex-row flex-wrap justify-between gap-4 bg-melon-700 p-6 text-center md:text-2xl">
       <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={0} end={20000} duration={1.2} isVisit />
+        <NumberCountUp start={disableAnimation ? 20000 : 0} end={20000} duration={disableAnimation ? 0 : 1.2} isVisit />
         <p>visits</p>
       </div>
       <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={0} end={15000} duration={1.2} />
+        <NumberCountUp start={disableAnimation ? 15000 : 0} end={15000} duration={disableAnimation ? 0 : 1.2} />
         <p>Students</p>
       </div>
       <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={0} end={2} duration={1.2} isDays />
+        <NumberCountUp start={disableAnimation ? 2 : 0} end={2} duration={disableAnimation ? 0 : 1.2} isDays />
         <p>of networking</p>
       </div>
     </div>
