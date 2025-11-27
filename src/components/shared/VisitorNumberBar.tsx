f05933bcc9e8bab81645d20e70a73e7ea00bbb2f
Neo Nguyen <neo20030125@gmail.com>
2025-11-27 23:04:55 +0100

    fix: max width on number bar content

--- src/components/shared/VisitorNumberBar.tsx
+++ src/components/shared/VisitorNumberBar.tsx
@@ -2,18 +2,20 @@ import { NumberCountUp } from "@/app/exhibitor/_components/NumberCountUp"
 
 export function VisitorNumberBar({ disableAnimation = false }: { disableAnimation?: boolean }) {
   return (
-    <div className="flex w-full flex-row flex-wrap justify-between gap-4 bg-melon-700 p-6 text-center md:text-2xl">
-      <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={disableAnimation ? 20000 : 0} end={20000} duration={disableAnimation ? 0 : 1.2} isVisit />
-        <p>visits</p>
-      </div>
-      <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={disableAnimation ? 15000 : 0} end={15000} duration={disableAnimation ? 0 : 1.2} />
-        <p>Students</p>
-      </div>
-      <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
-        <NumberCountUp start={disableAnimation ? 2 : 0} end={2} duration={disableAnimation ? 0 : 1.2} isDays />
-        <p>of networking</p>
+    <div className="w-full bg-melon-700 p-6">
+      <div className="mx-auto max-w-4xl flex flex-row justify-between text-center md:text-2xl">
+        <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
+          <NumberCountUp start={disableAnimation ? 20000 : 0} end={20000} duration={disableAnimation ? 0 : 1.2} isVisit />
+          <p>visits</p>
+        </div>
+        <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
+          <NumberCountUp start={disableAnimation ? 15000 : 0} end={15000} duration={disableAnimation ? 0 : 1.2} />
+          <p>Students</p>
+        </div>
+        <div className="w-3/12 font-bebas-neue font-medium text-stone-900">
+          <NumberCountUp start={disableAnimation ? 2 : 0} end={2} duration={disableAnimation ? 0 : 1.2} isDays />
+          <p>of networking</p>
+        </div>
       </div>
     </div>
   )
