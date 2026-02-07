ba69e3e0fbc4d7a89f5d70725be707964af391da
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-02-07 18:56:15 +0100

    fix: modal accessibility issues

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,7 +1,13 @@
 "use client"
 
 import { useScreenSize } from "@/components/shared/hooks/useScreenSize"
-import { Dialog, DialogContent } from "@/components/ui/dialog"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog"
 import { Drawer, DrawerContent } from "@/components/ui/drawer"
 import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
@@ -11,13 +17,17 @@ export default function Modal({
   open,
   setOpen,
   onClose,
-  className
+  className,
+  title = "Dialog",
+  description = "Dialog content"
 }: {
   children: ReactNode
   open: boolean
   setOpen: (open: boolean) => void
   onClose?: () => void
   className?: string
+  title?: string
+  description?: string
 }) {
   const { width } = useScreenSize()
 
@@ -31,6 +41,10 @@ export default function Modal({
   if (width && width > 768) {
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
+        <DialogHeader className="sr-only">
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
         <DialogContent
           className={cn("max-h-[80%] w-[80%] overflow-y-auto", className)}>
           {children}
