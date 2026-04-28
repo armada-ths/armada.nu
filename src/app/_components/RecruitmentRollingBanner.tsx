f6b1a98418abe09cf8756a127910012ba7e40221
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-04-28 20:19:56 +0200

    fix: prettier

--- src/app/_components/RecruitmentRollingBanner.tsx
+++ src/app/_components/RecruitmentRollingBanner.tsx
@@ -17,8 +17,7 @@ function formatTimeLeft(endDate: Date): string {
   const hours = Math.floor((totalSeconds % 86400) / 3600)
   const minutes = Math.floor((totalSeconds % 3600) / 60)
   const seconds = totalSeconds % 60
-  if (days > 3)
-    return `OT APPLICATIONS CLOSES IN ${days} DAYS`
+  if (days > 3) return `OT APPLICATIONS CLOSES IN ${days} DAYS`
   if (days > 0)
     return `OT APPLICATIONS CLOSES IN ${days}D ${hours}H ${minutes}M`
   if (hours > 0) return `OT APPLICATIONS CLOSES IN ${hours}H ${minutes}M`
