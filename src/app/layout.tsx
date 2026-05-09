a9d1e3003cd3b90eb9a5d274f8eeb2ba86a9b66d
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-05-10 00:12:10 +0200

    fix: image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,7 +68,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/public/screenshots/homepage_screenshot.png",
+        url: "/screenshots/homepage_screenshot.png",
         width: 2531,
         height: 1395,
         alt: "Armada homepage"
