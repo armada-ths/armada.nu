2115bd78c712e502913989a9c62db90d22860d32
Einar Harri <77612217+einhar1@users.noreply.github.com>
2026-05-21 09:28:09 +0200

    feat: robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        disallow: "/about/team",
+      },
+    ],
+    sitemap: "https://armada.nu/sitemap.xml",
+  }
+}
